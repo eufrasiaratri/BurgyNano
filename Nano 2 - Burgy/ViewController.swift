cc57e80cae68f7292f9bc9e3a617569900bea88e
Eufrasia <eufrasiaratrianggraini@gmail.com>
2022-07-25 14:37:38 +0700

    Initial Commit

---
+++ Nano 2 - Burgy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nano 2 - Burgy
+//
+//  Created by Eufrasia Ratri on 25/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
