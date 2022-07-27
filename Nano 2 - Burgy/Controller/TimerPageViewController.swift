eb0f8089a4e4e07fe66d9c0b6589d2ca5035d5fd
Bryan Kenneth <bryankenneth0309@gmail.com>
2022-07-27 09:11:42 +0700

    Update View Controlelr

---
+++ Nano 2 - Burgy/Controller/TimerPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimerPageViewController.swift
+//  Nano 2 - Burgy
+//
+//  Created by Bryan Kenneth on 27/07/22.
+//
+
+import UIKit
+
+class TimerPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
